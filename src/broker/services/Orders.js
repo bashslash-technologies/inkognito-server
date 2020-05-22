82fec2b7a2655a622b1fbeb05a142716c80c8904
Enoch Nana Nyankah Mensah <nyankahmensah@gmail.com>
2020-05-22 13:49:39 +0000

    working on order

--- src/broker/services/Orders.js
+++ src/broker/services/Orders.js
@@ -1,4 +1,4 @@
-const Utils = require("../utility");
+const { groupBy, entries, sumBy, map } = require("lodash");
 
 const OrderService = ({ ORM }) => {
 	//create an order
@@ -6,21 +6,35 @@ const OrderService = ({ ORM }) => {
 		try {
 			let __user = await ORM.Users.findById(user_id);
 			if(!__user) throw new Error("User not found")
-			let __cartTotal = 0;
-			let __cart = cart.map(async(item)=>{
-				let __product = await ORM.Products.findById(item.product);
-				if(!__product) throw new Error(`item ${item.product} does not exist`);
-				__cartTotal += (__product._id * item.quantity)
+			let __cart = cart.map(async({
+				product,
+				quantity
+			})=>{
+				let __product = await ORM.Products.findById(product);
+				if(!__product) throw new Error(`item ${product} does not exist`);
 				return ({
 					product: __product._id,
-					quantity: item.quantity,
+					vendor: __product.vendor,
+					price: __product.price,
+					quantity,
 				})
 			})
+			let __subcarts = [];
+			for (let [vendor, items] of entries(groupBy(__cart, 'vendor'))) {
+				let __suborder = new ORM.SubOrders({
+					vendor,
+					order: items,
+					cost: sumBy(items, ({price, quantity})=>(price*quantity)),
+				})
+				await __suborder.save();
+				__subcarts.push(__suborder);
+			}
 			let __order = new ORM.Orders({
 				user: user_id,
-				cart: __cart,
+				cart: map(__subcarts, '_id'),
 				cost: {
-					products: __cartTotal,
+					products: sumBy(__subcarts, 'cost'),
+					delivery: 'unknown'
 				},
 				transaction: {
 					account_holder,
@@ -36,10 +50,24 @@ const OrderService = ({ ORM }) => {
 		}
 	};
 
-	//retrieve all orders
+	//retrieve user orders
 	const read = async (user_id) => {
 		try {
 			return await ORM.Orders.find({user: user_id});
+		}
+		catch (err) {
+			throw err
+		}
+	};
+
+	//retrieve vendor orders
+	const readProvider = async (user_id) => {
+		try {
+			let __products = await ORM.Products({vendor: user_id});
+			let __product_list = __products.map((product)=>product._id)
+			return await ORM.Orders.find({
+				'cart.vendor': user_id,
+			});
 		}
 		catch (err) {
 			throw err
@@ -69,6 +97,7 @@ const OrderService = ({ ORM }) => {
 	return {
 		create,
 		read,
+		readProvider,
 		update,
 		cancel
 	};
