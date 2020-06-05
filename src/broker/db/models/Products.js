3438decf95b948302624d6ec5f123cb6dcd409a7
Domey Benjamin <domeybenjamin1@gmail.com>
2020-06-05 09:36:24 +0000

    Update:Added somethings

--- src/broker/db/models/Products.js
+++ src/broker/db/models/Products.js
@@ -1,40 +1,46 @@
 const mongoose = require("mongoose");
 
-const ProductSchema = new mongoose.Schema({
-	vendor: {
-		type: mongoose.SchemaTypes.ObjectId,
-		ref: 'users',
-		required: true,
-	},
-	name: {
-		type: String,
-		trim: true,
-		required: true,
-	},
-	images: [{
-		type: String,
-	}],
-	description: {
-		type: String,
-		trim: true,
-		required: true,
-	},
-	price: {
-		type: Number,
-		min: 0,
-		required: true,
-	},
-	stock: {
-		type: Number,
-		min: 0,
-	},
-	categories: [{
-		type: mongoose.Types.ObjectId,
-		ref: 'categories'
-	}],
-}, {
-	timestamps: true,
-	toJSON:{virtuals:true}
-});
+const ProductSchema = new mongoose.Schema(
+  {
+    vendor: {
+      type: mongoose.SchemaTypes.ObjectId,
+      ref: "users",
+      required: true,
+    },
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    description: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    price: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      min: 0,
+    },
+    categories: [
+      {
+        type: String,
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    toJSON: { virtuals: true },
+  }
+);
 
-module.exports = mongoose.model('products', ProductSchema);
+module.exports = mongoose.model("products", ProductSchema);
