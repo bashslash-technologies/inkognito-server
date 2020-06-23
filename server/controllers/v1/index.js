a4f8715ed8567707be43d4713259a349fddfeea6
Enoch Nana Nyankah Mensah <nyankahmensah@gmail.com>
2020-06-23 14:21:33 +0000

    fixed issues with courier

--- server/controllers/v1/index.js
+++ server/controllers/v1/index.js
@@ -3,11 +3,11 @@
 function apiV1Controller(io){
 	return {
 		categoryController: require('./categories')(io),
-		courierController: require('./trip')(io),
+		courierController: require('./courier')(io),
 		orderController: require('./order')(io),
 		productController: require('./product')(io),
 		shopController: require('./shop')(io),
-		tripController: require('./courier')(io),
+		tripController: require('./trip')(io),
 		userController: require('./user')(io),
 	}
 }
