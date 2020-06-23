631c806678c877e5ad724b157c1c579bf29b9d98
Enoch Nana Nyankah Mensah <nyankahmensah@gmail.com>
2020-06-23 14:39:41 +0000

    added middlewares

--- server/controllers/v1/courier.js
+++ server/controllers/v1/courier.js
@@ -3,12 +3,12 @@
 const express = require('express');
 const courierService = require('../../services/v1/courier');
 const storageService = require('../../services/v1/storage');
-const { handleSuccess, resolveVendor, resolveAdmin } = require('../../middlewares');
+const { handleSuccess, resolveCourier, resolveAdmin } = require('../../middlewares');
 
 function init(io) {
 	let router = express.Router();
 
-	router.post('/', resolveVendor, storageService.uploadCertificate().single("certificate"), async function(req, res, next) {
+	router.post('/', resolveCourier, storageService.uploadCertificate().single("certificate"), async function(req, res, next) {
 		try {
 			let result = await courierService.createCourier(req.user_id, req.body);
 			handleSuccess(res, result);
@@ -18,7 +18,7 @@ function init(io) {
 		}
 	});
 
-	router.get('/', resolveVendor, async function(req, res, next) {
+	router.get('/', resolveCourier, async function(req, res, next) {
 		try {
 			let result = await courierService.retrieveCourier(req.user_id, req.query);
 			handleSuccess(res, result);
@@ -38,7 +38,7 @@ function init(io) {
 		}
 	});
 
-	router.get('/wallet', resolveVendor, async function(req, res, next) {
+	router.get('/wallet', resolveCourier, async function(req, res, next) {
 		try {
 			let result = await courierService.checkBalance(req.user_id, req.params);
 			handleSuccess(res, result);
@@ -48,7 +48,7 @@ function init(io) {
 		}
 	});
 
-	router.post('/cashout', resolveVendor, async function(req, res, next) {
+	router.post('/cashout', resolveCourier, async function(req, res, next) {
 		try {
 			let result = await courierService.cashOut(req.user_id, req.body);
 			handleSuccess(res, result);
