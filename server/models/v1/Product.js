11225171ccf4a7d8339a0c6e9a2d51f3439a776a
Domey Benjamin <domeybenjamin1@gmail.com>
2020-06-23 12:25:36 +0000

    Update:added somethings

--- server/models/v1/Product.js
+++ server/models/v1/Product.js
@@ -1,13 +1,12 @@
-'use strict';
+"use strict";
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-<<<<<<< HEAD:src/broker/db/models/Products.js
 const ProductSchema = new mongoose.Schema(
   {
-    vendor: {
+    shop_id: {
       type: mongoose.SchemaTypes.ObjectId,
-      ref: "users",
+      ref: "shops",
       required: true,
     },
     name: {
@@ -32,62 +31,23 @@ const ProductSchema = new mongoose.Schema(
     },
     stock: {
       type: Number,
+      default: 0,
       min: 0,
     },
     categories: [
       {
-        type: String,
+        type: mongoose.Types.ObjectId,
+        ref: "categories",
       },
     ],
+    deleted: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true,
-    toJSON: { virtuals: true },
   }
 );
 
 module.exports = mongoose.model("products", ProductSchema);
-=======
-const ProductSchema = new mongoose.Schema({
-	shop_id: {
-		type: mongoose.SchemaTypes.ObjectId,
-		ref: 'shops',
-		required: true
-	},
-	name: {
-		type: String,
-		trim: true,
-		required: true,
-	},
-	images: [{
-		type: String,
-	}],
-	description: {
-		type: String,
-		trim: true,
-		required: true,
-	},
-	price: {
-		type: Number,
-		min: 0,
-		required: true,
-	},
-	stock: {
-		type: Number,
-		default: 0,
-		min: 0,
-	},
-	categories: [{
-		type: mongoose.Types.ObjectId,
-		ref: 'categories'
-	}],
-	deleted: {
-		type: Boolean,
-		default: false
-	}
-}, {
-	timestamps: true
-});
-
-module.exports = mongoose.model('products', ProductSchema);
->>>>>>> c6262b70c7ffa46cb057b3fd839a6b4b9fbe2e19:server/models/v1/Product.js
